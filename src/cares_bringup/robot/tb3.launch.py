e981c5e8e8b647ca6719498214d3d1e38329dd0a
Hiba Anwar <hiba2anwar@gmail.com>
2026-05-13 15:38:49 +0400

    update tb3 launch

--- src/cares_bringup/robot/tb3.launch.py
+++ src/cares_bringup/robot/tb3.launch.py
@@ -1,36 +1,61 @@
 import os
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, GroupAction
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import PushRosNamespace
+from launch.substitutions import LaunchConfiguration, Command
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
     tb3_bringup = get_package_share_directory('turtlebot3_bringup')
+    tb3_description = get_package_share_directory('turtlebot3_description')
+    lidar_pkg = get_package_share_directory('ld08_driver')
+
+    TURTLEBOT3_MODEL = os.environ.get('TURTLEBOT3_MODEL', 'burger')
+
     robot_id = LaunchConfiguration('robot_id')
 
-    declare_robot_id = DeclareLaunchArgument('robot_id', default_value='tb3_0')
-
-    hardware_group = GroupAction(
-        actions=[
-            PushRosNamespace(robot_id),
-            IncludeLaunchDescription(
-                PythonLaunchDescriptionSource(
-                    os.path.join(tb3_bringup, 'launch', 'robot.launch.py')
-                ),
-                launch_arguments={
-                    'namespace': robot_id,
-                }.items()
-            ),
-        ],
-        remappings=[
-            ('/tf', 'tf'),
-            ('/tf_static', 'tf_static'),
-        ]
-    )
+    tb3_param  = os.path.join(tb3_bringup, 'param', 'humble', f'{TURTLEBOT3_MODEL}.yaml')
+    urdf       = os.path.join(tb3_description, 'urdf', f'turtlebot3_{TURTLEBOT3_MODEL}.urdf')
+    robot_desc = Command(['xacro ', urdf, ' namespace:=', robot_id])
 
     return LaunchDescription([
-        declare_robot_id,
-        hardware_group
+        DeclareLaunchArgument('robot_id', default_value='tb3_0'),
+
+        # Robot State Publisher
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            namespace=robot_id,
+            parameters=[{'robot_description': robot_desc, 'use_sim_time': False}],
+            remappings=[('/tf', 'tf'), ('/tf_static', 'tf_static')],
+            output='screen'
+        ),
+
+        # LDS-02 LiDAR
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(lidar_pkg, 'launch', 'ld08.launch.py')
+            ),
+            launch_arguments={
+                'frame_id':  [robot_id, '/base_scan'],
+                'namespace': robot_id,
+            }.items()
+        ),
+
+        # TurtleBot3 Node
+        Node(
+            package='turtlebot3_node',
+            executable='turtlebot3_ros',
+            namespace=robot_id,
+            parameters=[
+                tb3_param,
+                {
+                    'diff_drive_controller.odometry.frame_id':       [robot_id, '/odom'],
+                    'diff_drive_controller.odometry.child_frame_id': [robot_id, '/base_footprint'],
+                }
+            ],
+            remappings=[('/tf', 'tf'), ('/tf_static', 'tf_static')],
+            output='screen'
+        ),
     ])
